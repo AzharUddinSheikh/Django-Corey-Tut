3f4c5a62da9975b4f345dbc6c7ec03623c438871
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2021-02-17 12:18:42 +0530

    ignored migration|cache files, created post model & registered in our blog admin, copied snippet for our Post view in base html and also provided few navigation

--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,13 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'azhar',
-        'title': 'Blog Post1',
-        'content': 'firs post content',
-        'date_posted': "august 27 2018"
-    },
-    {
-        'author': 'Valar',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': "may 01 2018"
-    },
-    {
-        'author': 'SilugGromRalab',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': "jan 14 2017"
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html',{'title':"About Author"})
+    return render(request, 'blog/about.html', {'title': "About Author"})
