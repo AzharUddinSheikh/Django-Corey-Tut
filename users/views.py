dbecf317c38f5a36a075224cae1ea5beded91fd5
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2021-02-17 21:20:43 +0530

    setting >> if user not accessed and tap on profile then it throw to login page, || views >> created decorator on profile function || added path for profile function || created profile html to view username who have accessed

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .form import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -9,12 +10,18 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created for {username}')
-            return redirect('blog-home')
+            messages.success(
+                request, f'Your Account Created for {username} Now Login')
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 
 # request.POST = request.POST is a data when we Request is POST and form ll created
 # replace UserCreationForm >> UserRegisterForm (bvoz its already a child class of that)
