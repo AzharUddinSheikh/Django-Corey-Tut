ad942f561378ab8d64d3c9bfa35a0a917b819837
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2021-02-17 23:57:55 +0530

    created model and registered it

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # if user is deleted then also delete the profile but if we delete the profile then user wont delte
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+        # this ll also change in database lets migrate
+        # error after trying make migrations its say instll pillow which works on images in python
