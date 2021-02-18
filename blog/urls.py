09bb1b7664bb0a1ef403558eb445caab68b7443a
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2021-02-18 23:22:52 +0530

    created class view LIST and DETAIL view also added path in urls.py and provided navigation in home ((if context not passed dango itself put object as default

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about')
 ]
+
+
+# we cant pass class like this PostListView to convert it into actual view we need this method as_view
+
+
+'''
+detailclass view 
+lets say we need to create a route to take us to a specific post 
+ex = let  say we want to see post for blog one that url something like post/1 
+so if you want to create a route where the id of a post is acutally part of the route
+then we can create a route 
+'''
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
-
+'''
+this is function based view
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+Class based view are better than function base as you see
+'''
 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': "About Author"})
+
+
+class PostListView(ListView):
+    # this is class based view djang0 support few of them like ListView , DetailView, delete update etc
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'  # what variable posts ll got to temp
+    ordering = ['-date_posted']  # newest to oldest
+    # ordering = ['date_posted'] oldest to newest
+
+    # if you forgot it then is show error like
+    # <app>/<models>_<viewtype>.html
+
+
+class PostDetailView(DetailView):
+    model = Post
