3968f0a8899091a41d17f2e2314307e9f1c3700e
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2021-02-18 19:18:16 +0530

    reduced image file uploaded by user

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -9,6 +10,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    # this func run after save it ll reduced the size of our image
+    def save(self):
+        super().save()
+
+        # first open the image
+        img = Image.open(self.image.path)
+        if img.height > 100 or img.width > 100:
+            output_size = (100, 100)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
         # this ll also change in database lets migrate
         # error after trying make migrations its say instll pillow which works on images in python
